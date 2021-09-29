6c9b5cf1f5da3892aea0d4acfc2c87cd37e4b42f
warcerg <warcerg@gmail.com>
2021-09-29 20:26:26 +0300

    Settings layout done

---
+++ app/src/main/java/com/example/spaceapp/framework/ui/settings/SettingsFragment.kt
@@ -0,0 +1,26 @@
+package com.example.spaceapp.framework.ui.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SettingsFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        fun newInstance() = SettingsFragment()
+    }
+}
